b6ccf7def6f68213b32dde32a02376f1f0402ece
veerendraravella <Vveerendra.ravella@gmail.com>
2022-10-31 22:32:13 +0530

    firstcommit

---
+++ src/Evenodd.java
@@ -0,0 +1,5 @@
+public class Evenodd {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/length.java
@@ -0,0 +1,7 @@
+public class  length {
+    public static void main(String[] args) {
+    int arr []={10,20,30,40,50};
+  System.out.print("the length of the given array= ");
+   System.out.println(arr.length);
+    }
+}
